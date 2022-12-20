748d994fcfa19a8c4d3d041690eac7db0b6c04ad
Chris Oakman <chris@oakmac.com>
2022-12-19 21:04:48 -0600

    use hiccups, add dom-util ns

--- src/hat_tourney_builder/core.cljs
+++ src/hat_tourney_builder/core.cljs
@@ -1,23 +1,50 @@
 (ns hat-tourney-builder.core
   (:require
-    [dumdom.core :as dumdom :refer [defcomponent]]
-    [goog.functions :as gfunctions]))
+    [oops.core :refer [oset!]]
+    ; [dumdom.core :as dumdom :refer [defcomponent]]
+    [goog.functions :as gfunctions]
+    [hiccups.runtime :as hiccups]
+    [hat-tourney-builder.util.dom :as dom-util :refer [set-inner-html!]])
+  (:require-macros
+    [hiccups.core :as hiccups :refer [html]]))
 
-(defcomponent heading
-  :on-render (fn [dom-node val old-val])
-  [data]
-  [:h2 {:style {:background "#000"}} (:text data)])
 
-(defcomponent HatTourneyBuilder [data]
+
+; (ns myns
+;   (:require-macros [hiccups.core :as hiccups :refer [html]])
+;   (:require [hiccups.runtime :as hiccups]))
+
+(hiccups/defhtml Foo1 []
   [:div
-    [heading (:heading data)]
-    [:p (:body data)]])
+    [:a {:href "https://github.com/weavejester/hiccup"}
+      "Hiccup"]])
+
+
+
+
+; (defcomponent heading
+;   :on-render (fn [dom-node val old-val])
+;   [data]
+;   [:h2 {:style {:background "#000"}} (:text data)])
+
+; (defcomponent HatTourneyBuilder [data]
+;   [:div
+;     [heading (:heading data)]
+;     [:p (:body data)]])
+
+(defn get-element [el-or-id-or-selector]
+  (js/document.getElementById el-or-id-or-selector))
+
+(defn set-html! [el html-str]
+  (oset! (get-element el) "innerHTML" html-str))
 
 (defn render! []
-  (dumdom/render
-    [HatTourneyBuilder {:heading {:text "Hat Tourney Builder"}
-                        :body "This is a web page 222"}]
-    (js/document.getElementById "appContainer")))
+  ; (dumdom/render
+  ;   [HatTourneyBuilder {:heading {:text "Hat Tourney Builder"}
+  ;                       :body "This is a web page 222"}]
+  ;   (js/document.getElementById "appContainer"))
+
+  (set-inner-html! "appContainer" (Foo1)))
 
 (defn init!* []
   (js/console.log "Initialized Tourney Hat Builder 😎")
---
+++ src/hat_tourney_builder/util/dom.cljs
@@ -0,0 +1,85 @@
+(ns hat-tourney-builder.util.dom
+  (:require
+    [goog.dom :as gdom]
+    [goog.object :as gobj]))
+
+(defn query-select [q]
+  (.querySelector js/document q))
+
+(defn query-select-all [q]
+  (.querySelectorAll js/document q))
+
+(defn safe-prevent-default
+  "calls preventDefault() on a JS event if possible"
+  [js-evt]
+  (when (fn? (gobj/get js-evt "preventDefault"))
+    (.preventDefault js-evt)))
+
+(defn get-element
+  "does it's best to grab a native DOM element from it's argument
+  arg can be either:
+  1) already a DOM element
+  2) id of an element (getElementById)
+  3) querySelector
+
+  return nil if not able to grab the element"
+  [arg]
+  (let [el1 (gdom/getElement arg)]
+    (if el1
+      el1
+      (let [el2 (.querySelector js/document arg)]
+        (if el2 el2 nil)))))
+
+(defn xy-inside-element?
+  [el x y]
+  (let [js-box (.getBoundingClientRect el)
+        left (gobj/get js-box "left")
+        width (gobj/get js-box "width")
+        height (gobj/get js-box "height")
+        top (gobj/get js-box "top")]
+    (and (>= x left)
+         (< x (+ left width))
+         (>= y top)
+         (< y (+ top height)))))
+
+(defn get-height
+  [el]
+  (-> (.getBoundingClientRect el)
+      (gobj/get "height")))
+
+(defn get-width
+  [el]
+  (-> (.getBoundingClientRect el)
+      (gobj/get "width")))
+
+(defn set-style-prop!
+  [el prop value]
+  (-> (get-element el)
+      (gobj/get "style")
+      (gobj/set prop value)))
+
+(defn set-inner-html!
+  [el html]
+  (-> (get-element el)
+      (gobj/set "innerHTML" html)))
+
+(defn append-html!
+  [el additional-html]
+  (-> (get-element el)
+      (.insertAdjacentHTML "beforeend" additional-html)))
+
+(defn add-class!
+  [el classname]
+  (-> (get-element el)
+      (gobj/get "classList")
+      (.add classname)))
+
+(defn remove-class!
+  [el classname]
+  (-> (get-element el)
+      (gobj/get "classList")
+      (.remove classname)))
+
+(defn remove-element!
+  [el]
+  (gdom/removeNode (get-element el)))
